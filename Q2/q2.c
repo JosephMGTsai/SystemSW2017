8ee2cfd36a4ec260fda8daf300eaaf9774b7ce0a
Joseph M. G. Tsai <mgtsai@gmail.com>
2017-07-14 12:44:36 +0000

    Refine Q2 multiplication function

--- Q2/q2.c
+++ Q2/q2.c
@@ -3,27 +3,27 @@
 #include <stdlib.h>
 
 
-uint64_t mult_uint32(uint32_t x, uint32_t y, uint32_t a, uint32_t b, uint64_t bit, uint64_t result)
+uint64_t x_mul_y_add_a_add_b(uint32_t x, uint32_t y, uint32_t a, uint32_t b)
 {
-    if (bit == 0)
-        return result;
+    if ((y | a | b) == 0)
+        return 0;
 
-    uint32_t c = y & bit ? x : 0;
+    uint32_t c = y & 1 ? x : 0;
     uint32_t ab = a ^ b;
 
     uint32_t sum = ab ^ c;
     uint32_t carry = (a & b) | (ab & c);
 
-    return mult_uint32(x, y, sum >> 1, carry, bit << 1, result | (sum & 1 ? bit : 0));
+    return (x_mul_y_add_a_add_b(x, y >> 1, sum >> 1, carry) << 1) | (sum & 1);
 }
 
 
 int main(void)
 {
     for (int i = 0; i < 100; ++i) {
-        uint32_t x = (rand() << 1) ^ rand();
-        uint32_t y = (rand() << 1) ^ rand();
-        uint64_t xy = mult_uint32(x, y, 0, 0, 1, 0);
+        uint32_t x = (rand() << 16) ^ rand();
+        uint32_t y = (rand() << 16) ^ rand();
+        uint64_t xy = x_mul_y_add_a_add_b(x, y, 0, 0);
         uint64_t expected = (uint64_t) x * (uint64_t) y;
 
         printf("0x%08x * 0x%08x = 0x%08x%08x (expected: 0x%08x%08x, equals: %s)\n", x, y, (uint32_t) (xy >> 32), (uint32_t) xy, (uint32_t) (expected >> 32), (uint32_t) expected, xy == expected ? "TRUE" : "FALSE");
