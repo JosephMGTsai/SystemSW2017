b3af6e1b3fc87c4c823fcf8c8183a12a3f5eb5d1
Joseph M. G. Tsai <mgtsai@gmail.com>
2017-07-14 06:09:27 +0000

    Add uint16_t implementation for Q1

--- Q1/q1.c
+++ Q1/q1.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 
-/* ==== Original function
-uint32_t func(uint32_t x) {
+uint32_t func(uint32_t x)
+{
     uint32_t n = x;
     n = ((n & 0xffff0000) >> 16) | ((n & 0x0000ffff) << 16);
     n = ((n & 0xff00ff00) >>  8) | ((n & 0x00ff00ff) <<  8);
@@ -13,10 +13,9 @@ uint32_t func(uint32_t x) {
     n = ((n & 0xaaaaaaaa) >>  1) | ((n & 0x55555555) <<  1);
     return n;
 }
-*/
 
 
-uint32_t func(uint32_t x)
+uint32_t func_uint32(uint32_t x)
 {
     uint32_t r = 1, n = 0;
     for (; r; r <<= 1)
@@ -25,12 +24,21 @@ uint32_t func(uint32_t x)
 }
 
 
+uint16_t func_uint16(uint16_t x)
+{
+    uint32_t r = 1, n = 0;
+    for (; r & 0xffff; r <<= 1)
+        n = (n << 1) | !!(x & r);
+    return (uint16_t) n;
+}
+
+
 int main(void)
 {
     for (int i = 0; i < 10; ++i) {
-        uint32_t x = rand();
-        uint32_t n = func(x);
-        printf("%10u 0x%08x => %10u 0x%08x\n", x, x, n, n);
+        uint16_t x = (uint16_t) rand();
+        uint16_t n = func_uint16(x);
+        printf("%5u 0x%04x => %5u 0x%04x\n", x, x, n, n);
     }
     return 0;
 }
